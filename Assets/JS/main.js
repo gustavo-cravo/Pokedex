5089b1db32dec49117e83d49efbed78dea35a14e
Gustavo <gustavocravo58@gmail.com>
2025-12-23 10:52:54 -0300

    Transformando a lista de Pokemon em uma lista de HTML
    Foi adicionado um id na lista do index.html, em seguida foi feito uma função para adicionar o texto em HTML com a variável do nome que eu peguei do retorno do jsonBody.results. E criado um laço de repetição para adicionar os resultados dessa array que me retornou na função com o texto em html

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -3,6 +3,27 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
+// Esta função vai gerar o texto da lista em html Inicialmnente apenas com os dados dos nomes
+function convertHTML(pokemon){
+    return `<li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+
+// Seleciona a lista dos pokemons pelo ID e armazena na variável
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
     //Este primeiro vai pegar o retorno da minha requisição do fetch e converter em um json.
     .then(function(response){
@@ -10,7 +31,15 @@ fetch(url)
     })
     // Aqui ele vai pegar o retorno do primeiro then e mostrar no console.
     .then(function(jsonBody){
-        console.log(jsonBody);
+        return jsonBody.results;
+    })
+    // Vai pegar o retorno da anterior e iniciar uma repetição para adicionar no HTML as listas dos próximos pokemons
+    .then(function(pokemons){
+        for(let i = 0; i < pokemons.length; i++)
+        {
+            let pokemon = pokemons[i];
+            pokemonList.innerHTML += convertHTML(pokemon);
+        }
     })
     // Caso dê alguma mensagem de erro ele vai executar o catch
     .catch(function(error){
