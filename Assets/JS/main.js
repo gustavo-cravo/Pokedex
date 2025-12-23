f70003390d57424f539fccd151969d5f049cb9ff
Gustavo <gustavocravo58@gmail.com>
2025-12-23 09:52:01 -0300

    Manipulando o resultado de uma requisição através de uma promise
    Após buscar na url com o fetch, o resultado foi adicionado no parâmetro da primeira function e no corpo convertido em um json, no segundo then ele pega esse retorno e mostra no console o resultado do json.

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -3,9 +3,19 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response){
-    console.log(response);
-})
-
-let x = 10 + 10
-console.log(x);
+fetch(url)
+    //Este primeiro vai pegar o retorno da minha requisição do fetch e converter em um json.
+    .then(function(response){
+        return response.json();
+    })
+    // Aqui ele vai pegar o retorno do primeiro then e mostrar no console.
+    .then(function(jsonBody){
+        console.log(jsonBody);
+    })
+    // Caso dê alguma mensagem de erro ele vai executar o catch
+    .catch(function(error){
+        console.error(error);
+    })
+    .finally(function(){
+        console.log('Requisição concluída!')
+    })
