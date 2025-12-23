c57d674d9db955b78ab4c84686ad74ca43c9cd34
Gustavo <gustavocravo58@gmail.com>
2025-12-23 15:12:28 -0300

    Manipulando múltiplas requisições em paralelo
    Os arquivos main.js e poke-api.js foram atualizados para buscar dinamicamente os detalhes dos Pokémon na PokeAPI, incluindo tipos e imagens, e renderizá-los no HTML. A marcação estática dos Pokémon foi removida do arquivo index.html para permitir a geração de conteúdo dinâmico.

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,16 +1,21 @@
+function convertPokemonTypesToList(pokemonTypes){
+    return pokemonTypes.map((typeSlot)=> {
+        return `<li class = "type">${typeSlot.type.name}</li>`
+    })
+}
+
 // Esta função vai gerar o texto da lista em html Inicialmnente apenas com os dados dos nomes
 function convertHTML(pokemon){
     return `<li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToList(pokemon.types).join('')}
                     </ol>
 
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
                         alt="${pokemon.name}">
                 </div>
             </li>`
--- Assets/JS/poke-api.js
+++ Assets/JS/poke-api.js
@@ -1,18 +1,25 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemns = function(offset = 0, limit = 10){
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
     //Este primeiro vai pegar o retorno da minha requisição do fetch e converter em um json.
-    .then(function(response){
-        return response.json();
-    })
-    // Aqui ele vai pegar o retorno do primeiro then e mostrar no console.
-    .then(function(jsonBody){
-        return jsonBody.results;
-    })
+    .then((response) => response.json())
+    // Aqui ele vai pegar o retorno do primeiro then e mostrar no console apenas os results.
+    .then((jsonBody) => jsonBody.results)
+    // Está mapeando a lista em uma lista de requisições dos detalhes dos pokemons.
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    // Aguardando terminar todas as requisições
+    .then((detailRequests) => Promise.all(detailRequests))
+    // Quando terminar ele vai entregar uma lista de detalhes dos pokemons.
+    .then((pokemonDetails) => pokemonDetails)
     .catch(function (error){
         console.error(error);
     })
