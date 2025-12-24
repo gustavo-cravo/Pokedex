e31d33c5a0d92bd96c20793b9e77fa99e241bad7
Gustavo <gustavocravo58@gmail.com>
2025-12-24 15:40:55 -0300

    Reformule a interface modal e ajuste o limite da lista de Pokémon.
    Redesenhamos a sobreposição modal em index.html e atualizamos sua estrutura para melhorar a aparência e a clareza. Adicionamos estilos modais abrangentes em pokedex.css, incluindo novas classes e animações. Alteramos o limite de busca da lista de Pokémon de 10 para 3 em main.js para um melhor controle de paginação.

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,6 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 10
+const limit = 3
 let offset = 0
 const maxRecords = 151
 
