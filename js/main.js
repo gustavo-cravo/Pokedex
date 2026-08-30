5be511469ce438da64ae8dd5aef073eaf67d6405
Gustavo <gustavocravo58@gmail.com>
2026-08-29 21:34:16 -0300

    Realizando as requisições e filtrando os dados dos pokemons.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-requisicoes = [];
+import {Pokemon} from "./pokemon.js";
 
-try{
-    async function buscarPokemon(){
-        for(let id = 1; id <= 25; id++){
-            requisicoes.push(fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-            .then(response => response.json()));
-            console.log(requisicoes);
-        }
+async function buscarPokemon(){
+    try{
+            const requisicoes = [];
+
+            for(let id = 1; id <= 2; id++){
+                requisicoes.push(fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+                .then(response => response.json()));
+            }
+
+            const pokemon = await Promise.all(requisicoes);
+
+            return pokemon;
+    }catch(erro){
+        console.error('Erro ao buscar Pokémon:', erro);
+        console.log(erro)
     }
-}catch(erro){
-    console.error('Erro ao buscar Pokémon:', erro);
-}
+} 
+
+const pokemonsBuscados = await buscarPokemon();
+
+const pokemosFiltrados = pokemonsBuscados.map(pokemonAtual => {
+    
+   return {
+    id: pokemonAtual.id,
+    name: pokemonAtual.name,
+    types: pokemonAtual.types.map(item => {
+        return item.type.name
+   }),
+   image: pokemonAtual.sprites.other["home"].front_default}
+});
+
+console.log(pokemosFiltrados);
